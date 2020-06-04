421bc4ba11040beb6d0dd62ccb12be0996814e88
Riley-Robinson <rrtoinfinity@gmail.com>
2020-06-04 17:52:30 -0400

    something is broken but thunder storm commit

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const server = require("./server.js");
 
 const port = 8000;
-server.listen(port, () => {
-    console.log(`\n== Server Running on http://localhost:${port} ==\n`);
-});
+server.listen(port, () => console.log(`\n== Server Running on http://localhost:${port} ==\n`))
